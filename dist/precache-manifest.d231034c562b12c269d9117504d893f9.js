971dd1f086ff01d383775076cf43acb028e025ad
rahul <rahul.adsps@gmail.com>
2020-12-19 09:17:55 -0800

    adding dist

---
+++ dist/precache-manifest.d231034c562b12c269d9117504d893f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d887ba6ce41ba163a55",
+    "url": "/css/app.8607edc3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dbe650576aef8347f6b0efd91be2b0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf3844832fd9349d9fa",
+    "url": "/js/about.6f727b79.js"
+  },
+  {
+    "revision": "0d887ba6ce41ba163a55",
+    "url": "/js/app.7e37a04d.js"
+  },
+  {
+    "revision": "942a841bdc26234fe999",
+    "url": "/js/chunk-vendors.8380736d.js"
+  },
+  {
+    "revision": "be13261b1bf8fcc119474848038d9be1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
